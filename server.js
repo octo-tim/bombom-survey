42a06da7441d5c81ed0dffcc7ffd3e108b15106d
octo-tim <timyun816@gmail.com>
2026-04-14 10:17:53 +0900

    Railway 배포: Express 정적 서버 구성

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`봄봄매트 설문조사 서버 실행: port ${PORT}`);
+});
